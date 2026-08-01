46fef92900273c3881f19b680947874952d10aab
Phát Nguyễn Tuấn <phatnt5052@ut.edu.vn>
2026-08-01 09:33:18 +0700

    Add TcpClientService class with TODOs for functionality

---
+++ Client/ClientApp/Services/TcpClientService.cs
@@ -0,0 +1,10 @@
+namespace ClientApp.Services
+{
+    public class TcpClientService
+    {
+        // TODO: Connect to server
+        // TODO: Send LIST command
+        // TODO: Send GET command
+        // TODO: Receive data
+    }
+}
