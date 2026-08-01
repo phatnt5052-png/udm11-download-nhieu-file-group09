578a23e55174c9128249ca63aee9fdc822c62ec7
Phát Nguyễn Tuấn <phatnt5052@ut.edu.vn>
2026-08-01 09:35:34 +0700

    Create DownloadService class with TODOs
    Added DownloadService class with TODOs for file download features.

---
+++ Client/ClientApp/Services/Client/ClientApp/Services/DownloadService.cs
@@ -0,0 +1,9 @@
+namespace ClientApp.Services
+{
+    public class DownloadService
+    {
+        // TODO: Download single file
+        // TODO: Save file
+        // TODO: Report progress
+    }
+}
