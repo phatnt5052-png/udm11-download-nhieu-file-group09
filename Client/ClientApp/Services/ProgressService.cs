0bed519a21843f2062ec40b1861760b194fadcab
tranquocphong113 <phongtq063@gmail.com>
2026-08-10 00:33:11 +0700

    DownloadQueueService va ProgressService

--- Client/ClientApp/Services/ProgressService.cs
+++ Client/ClientApp/Services/ProgressService.cs
@@ -1,7 +1,33 @@
+using ClientApp.Models;
 namespace ClientApp.Services
 {
     public class ProgressService
     {
-        // TODO
+        public event EventHandler<DownloadItem> ProgressChanged;
+        private readonly Dictionary<string, (long bytes, DateTime time)> _lastUpdate = new();
+        
+        public void UpdateProgress(DownloadItem item, long downloadedBytes)
+        {
+            item.DownloadedBytes = downloadedBytes;
+            if (item.FileSize > 0)
+            {
+                item.Progress = (double)downloadedBytes / item.FileSize * 100;
+            }
+
+            DateTime now = DateTime.Now;
+            if (_lastUpdate.TryGetValue(item.FileName, out var last))
+            {
+                double secondsElapsed = (now - last.time).TotalSeconds;
+                if (secondsElapsed > 0)
+                {
+                    long bytesSinceLast = downloadedBytes - last.bytes;
+                    double mbSinceLast = bytesSinceLast / 1024.0 / 1024.0;
+                    item.SpeedMbps = mbSinceLast / secondsElapsed;
+                }
+            }
+            _lastUpdate[item.FileName] = (downloadedBytes, now);
+            ProgressChanged?.Invoke(this, item);
+        }
+        
     }
 }
