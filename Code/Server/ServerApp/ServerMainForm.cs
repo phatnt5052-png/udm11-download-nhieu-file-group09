61622136263049ed7f0d129889e8b2534174db77
phucnt5429-design <phucnt5429@ut.edu.vn>
2026-08-18 11:45:48 +0700

    Add initial ServerMainForm implementation

--- Code/Server/ServerApp/ServerMainForm.cs
+++ Code/Server/ServerApp/ServerMainForm.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Windows.Forms;
 
+namespace ServerApp
+{
+    public partial class ServerMainForm : Form
+    {
+        public ServerMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ServerMainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
